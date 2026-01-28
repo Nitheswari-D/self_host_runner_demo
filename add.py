9af7bf406f3cec82b5ae7c6e0260b9bd046bf157
Nitheswari-D <nitheswarid2003@gmail.com>
2026-01-28 15:49:00 +0530

    pytestv5

--- add.py
+++ add.py
@@ -2,7 +2,7 @@ def add_numbers(num1,num2):
     sum_result = num1+num2
     return sum_result
 
-number1 = 10
+number1 = 11
 number2 = 20
 result = add_numbers(number1, number2)
 print(f"The sum of {number1} and {number2} is: {result}")
