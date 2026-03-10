567f558bcd7b94fbd331954bf3f0d2d4e6ac9489
Nitheswari-D <nitheswarid2003@gmail.com>
2026-03-10 02:49:13 -0700

    patch_v1

--- add.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(num1,num2):
-    sum_result = num1+num2
-    return sum_result
-
-number1 = 11
-number2 = 20
-result = add_numbers(number1, number2)
-print(f"The sum of {number1} and {number2} is: {result}")
