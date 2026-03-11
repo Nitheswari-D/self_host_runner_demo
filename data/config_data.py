68915ea5ceeba5c2f884b12437031c6aa336f344
Nitheswari-D <nitheswarid2003@gmail.com>
2026-03-11 02:30:54 -0700

    update_v12

--- data/config_data.py
+++ data/config_data.py
@@ -7,8 +7,6 @@ RECORDING_DURATION = 60
 IP_ADDRESS = "192.168.0.103"
 DEVICE_INFO_FILE_PATH = r"data\device_data.json"
 
-
-
 #LED Enumeration
 CAMERA_DEVICE_NAME = "Brio 501"
 REFERENCE_IMG_PATH  = r"data\Images\Reference_img.jpg"  
